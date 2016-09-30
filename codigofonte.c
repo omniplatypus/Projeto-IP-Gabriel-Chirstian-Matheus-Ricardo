57ebf07c2ef60df53eaf3692868a22cfe2438f71
omniplatypus <gmelo.cc@gmail.com>
2016-09-30 20:44:34 -0300

    Update codigofonte.c

--- codigofonte.c
+++ codigofonte.c
@@ -1,3 +1,15 @@
 #include <stdio.h>				//comentar o código faz bem :D
 #include <stdlib.h>
 #include <string.h>
+
+#typedef struct
+{
+	char nome[30];
+	float preço;
+	int estoque;
+}Produto;
+
+int main()
+{
+	return 0;
+}
