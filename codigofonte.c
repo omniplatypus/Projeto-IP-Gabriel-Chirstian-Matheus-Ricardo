d3b2eaddf1fdeb411a0a28436adab51ab873f85b
omniplatypus <gmelo.cc@gmail.com>
2016-09-30 21:08:28 -0300

    Update codigofonte.c

--- codigofonte.c
+++ codigofonte.c
@@ -1,15 +1,19 @@
 #include <stdio.h>				//comentar o código faz bem :D
 #include <stdlib.h>
 #include <string.h>
-
-#typedef struct
+typedef struct
 {
 	char nome[30];
-	float preço;
+	char ingrediente[10][20];		//supus que 10 ingredientes por produto seriam representados assim.
+	float preco;
 	int estoque;
+	int validade[3];
 }Produto;
 
 int main()
 {
+	Produto pizza;
+	gets(pizza.ingrediente[0]);
+	printf("%s",pizza.ingrediente[0]);
 	return 0;
 }
